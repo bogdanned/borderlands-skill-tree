67834e392577c3cc15a294c34b4b37c98e2d9ff1
Bogdan Nedelcu <bogdanned32@gmail.com>
2024-07-12 17:11:25 -0400

    feat: updated icons

--- src/App.js
+++ src/App.js
@@ -5,20 +5,41 @@ import { SkillProvider, SkillTreeGroup, SkillTree } from "beautiful-skill-tree";
 import { motion, harmony, cataclysm } from './data/data';
 import theme from './data/theme';
 
+function handleSave(
+  storage,
+  treeId,
+  skills
+) {
+  console.log(treeId, skills)
+  return storage.setItem(`skills-${treeId}`, JSON.stringify(skills));
+}
+
+// the state of the skill tree, as per my custom implementation
+const savedData = {
+  'ward': {
+    optional: false,
+    nodeState: 'selected',
+  },
+  'suspension': {
+    optional: false,
+    nodeState: 'selected',
+  },
+  'converge': {
+    optional: true,
+    nodeState: 'selected'
+  }
+};
+
 function App() {
   return (
     <div className="App">
-      <div style={{ width: "500px", height: '150px', margin: '0 auto' }}>
-        <img style={{ width: "500px" }} src={logo} alt="borderlands logo." />
-      </div>
+
       <SkillProvider>
         <SkillTreeGroup theme={theme}>
           {() => {
             return (
               <React.Fragment>
-                <SkillTree title="Motion" treeId="motion" data={motion} />
-                <SkillTree title="Harmony" treeId="harmony" data={harmony} />
-                <SkillTree title="Cataclysm" treeId="cataclysm" data={cataclysm} />
+                <SkillTree title="Frontend Mastery" treeId="frontend-mastery" data={motion} handleSave={handleSave} savedData={savedData}/>
               </React.Fragment>
             );
           }}
